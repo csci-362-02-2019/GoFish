a74abd905063ae0cf792dfad6d6635ff9fec80e4
Steven Higgins <stevenhiggins@stevens-mbp.wireless.cougars.int>
2019-10-08 13:50:20 -0400

    Driver Structure added

--- testCasesExecutables/ContrastFinder/src/com/csci362/gofish/xColorNameLookupDriver.java
+++ testCasesExecutables/ContrastFinder/src/com/csci362/gofish/xColorNameLookupDriver.java
@@ -2,5 +2,7 @@ package com.csci362.gofish;
 public class xColorNameLookupDriver {
 	public static void main (String[] args){
 		ColorNameLookup obj = new ColorNameLookup();
-	}
+		obj.getColorFromName("YeLLow");
+		System.out.println(obj);
+}
 }
