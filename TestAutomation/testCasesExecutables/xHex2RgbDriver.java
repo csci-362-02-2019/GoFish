8819c67a99e6a211d21b3f5881c018950f7ae9f7
Century <haganlg@g.cofc.edu>
2019-10-24 12:23:24 -0400

    Fixed hex2Rgb driver

--- TestAutomation/testCasesExecutables/xHex2RgbDriver.java
+++ TestAutomation/testCasesExecutables/xHex2RgbDriver.java
@@ -1,11 +1,27 @@
-//package com.csci362.gofish;
+import java.io.File;
+import java.util.Scanner;
+import java.awt.Color; 
+
+
 public class xHex2RgbDriver {
 	
 	public static void main(String[] agrs) {
-		
-		System.out.println("Test");
+		try {
+			boolean correct = false;		
+			String input = agrs[0];
+			String output;
+			String oricle = agrs[1];
 
-	}
+				
+			ColorConverter obj = new ColorConverter();
+
+			output = obj.hex2Rgb(input);
+			System.out.println(output);
+		}catch{
+			System.out.println0("AAAAAAAAAAaaaAAAaaaaAAAAAAAAAAaaaaaaa");
+		}
+							
+	}		
+}
 
 	
-}
