0bf04015c6b5da2f1d1614a1d3e80c61036a3414
higginssteven98 <higgiss2@g.cofc.edu>
2019-10-29 11:22:41 -0400

    fixed hex2RgbDriver

--- TestAutomation/testCasesExecutables/xHex2RgbDriver.java
+++ TestAutomation/testCasesExecutables/xHex2RgbDriver.java
@@ -4,16 +4,28 @@ import java.awt.Color;
 
 public class xHex2RgbDriver {
 	
-	public static void main(String[] agrs) {
+	public static void main(String[] args) {
 		try {
 			boolean correct = false;		
-			String input = agrs[0];
-			String orcale = agrs[1];
+			String id = args[0];
+			String requirement = args[1];
+			String driver = args[2];
+			String component = args[3];
+			String method = args[4];
+			String input = args[5];
+			String answer = args[6];
 				
 			ColorConverter obj = new ColorConverter();
 
 			Color output = obj.hex2Rgb(input);
 
+			System.out.println (id);
+			System.out.println (requirement);
+			System.out.println (driver);
+			System.out.println (component);
+			System.out.println (method);
+			System.out.println (input);
+			System.out.println (answer);
 			System.out.println(obj.hex2Rgb(output));
 }
 		catch (Exception e){
